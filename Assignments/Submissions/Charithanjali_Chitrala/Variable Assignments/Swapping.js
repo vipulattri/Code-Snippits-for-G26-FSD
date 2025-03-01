debc2e2910de6d21bc00735bc5ad16badb4c0f5a
Charitha <162724665+Charitha-19@users.noreply.github.com>
2025-03-01 17:33:02 +0530

    Update Swapping.js

--- Assignments/Submissions/Charithanjali_Chitrala/Variable Assignments/Swapping.js
+++ Assignments/Submissions/Charithanjali_Chitrala/Variable Assignments/Swapping.js
@@ -1,1 +1,20 @@
+// START
+//   SET a = 5, b = 10
+//    Step 1: Add both values
+//    Step 2: Assign new value to b
+//    Step 3: Assign new value to a
+//   PRINT "a:", a, "b:", b
+// END
 
+
+let a = 5, b = 10;
+a = a + b;  
+b = a - b; 
+a = a - b;  
+console.log("a:", a, "b:", b); 
+
+
+
+let d = 12, c = 19;
+[d, c] = [c, d]; 
+console.log("d:", d, "c:", c); 
