92d56c25411778634ae72bba67a7525f8d2de06a
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-03-02 14:36:18 +0530

    Update Basic Calculator.js

--- Assignments/Submissions/prasanth82006/Variable Assignments/Basic Calculator.js
+++ Assignments/Submissions/prasanth82006/Variable Assignments/Basic Calculator.js
@@ -19,13 +19,3 @@ switch (operations) {
     result = "Invalid Operations";
 }
 console.log(`Result: ${result}`);
-
-// swapping of numbers
-
-let a=6
-let b=10
-a=a+b
-b=a-b
-a=a-b
-console.log(`The A value is:${a}`);
-console.log(`The B value is:${b}`);
