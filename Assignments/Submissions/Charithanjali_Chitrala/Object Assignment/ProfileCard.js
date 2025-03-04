1df7739464b82ab90db3268f9e72e3d45d9fe649
Charitha <162724665+Charitha-19@users.noreply.github.com>
2025-03-04 19:17:28 +0530

    Update ProfileCard.js

--- Assignments/Submissions/Charithanjali_Chitrala/Object Assignment/ProfileCard.js
+++ Assignments/Submissions/Charithanjali_Chitrala/Object Assignment/ProfileCard.js
@@ -1,1 +1,24 @@
+// START  
+// SET userDetails = { name: "Charithanjali", age: 20, city: "Kurnool" }  
+
+// SET userDetails.age = 21  
+// SET userDetails.profession = "Student"  
+// REMOVE userDetails.city  
+
+// PRINT userDetails  
+// END  
+
+
+
+
+
+let userDetails = {
+    name: "Charithanjali",
+    age: 20,
+    city: "Kurnool"
+};
+userDetails.age = 21;
+userDetails.profession = "Student";
+delete userDetails.city;
+console.log(userDetails);
 
