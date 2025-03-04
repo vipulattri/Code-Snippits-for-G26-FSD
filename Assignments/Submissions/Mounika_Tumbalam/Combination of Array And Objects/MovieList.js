9f0e7899d03d581908adf7a2ccb66cab71424883
Mounika-tumbalam <tumbalammounika@gmail.com>
2025-03-04 22:11:50 +0530

    Add files via upload

---
+++ Assignments/Submissions/Mounika_Tumbalam/Combination of Array And Objects/MovieList.js
@@ -0,0 +1,18 @@
+// Create an array of objects, each containing movieName, rating, and year.
+
+const MovieList = [
+    {Moviename:"Bahubali",
+        year:2017,
+        rating:8.2
+    },
+    {Moviename:"Jersey",
+       year:2019,
+        rating:8.5
+    },
+    {Moviename:"pushpa:The Rise",
+       year:2021,
+        rating:7.9
+    },
+];
+
+console.log("MovieList: ",MovieList);
