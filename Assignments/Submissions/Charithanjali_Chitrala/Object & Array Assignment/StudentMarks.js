04d6de7865dd0d2810c5bef89c8d900701779fca
Charitha <162724665+Charitha-19@users.noreply.github.com>
2025-03-06 18:39:09 +0530

    Update StudentMarks.js

--- Assignments/Submissions/Charithanjali_Chitrala/Object & Array Assignment/StudentMarks.js
+++ Assignments/Submissions/Charithanjali_Chitrala/Object & Array Assignment/StudentMarks.js
@@ -1,1 +1,36 @@
+// START  
+// DECLARE students as a list of objects (name, marks)  
+// DECLARE totalMarks as 0  
+// DECLARE highestStudent as first student in the list  
+// FOR each student in students  
+//     ADD student.marks to totalMarks  
+//     IF student.marks > highestStudent.marks  
+//         UPDATE highestStudent with current student  
+// CALCULATE averageMarks as totalMarks divided by number of students  
+// PRINT "Average Marks:", averageMarks  
+// PRINT "Top Student:", highestStudent.name, "with", highestStudent.marks, "marks"  
+// END  
+
+
+
+
+
+let students = [
+    { name: "Affu", marks: 85 },
+    { name: "Bhavya", marks: 92 },
+    { name: "Charitha", marks: 78 },
+    { name: "Vaishu", marks: 88 },
+    { name: "Anjali", marks: 95 }
+];
+let totalMarks = 0;
+let highestStudent = students[0];
+for (let student of students) {
+    totalMarks += student.marks;
+    if (student.marks > highestStudent.marks) {
+        highestStudent = student;
+    }
+}
+let averageMarks = totalMarks / students.length;
+console.log("Average Marks:", averageMarks);
+console.log("Top Student:", highestStudent.name, "with", highestStudent.marks, "marks");
 
