1c011eb1de7b2ac611f90408a6e698247aaf07ac
Lakshmi <gurammanavarl@gmail.com>
2025-03-08 01:11:54 +0530

    Add files via upload

---
+++ JsSubmissions/lakshmifg/Array Assignments/To-DoList.js
@@ -0,0 +1,13 @@
+let toDoList = [];
+
+toDoList.push("hi im lakshmi");
+toDoList.push("finished diploma 98%");
+toDoList.push("want to go to good colleg");
+
+console.log("Initial to-do list:", toDoList);
+
+toDoList.shift(); // Remove the first item
+
+toDoList.push("Learn JavaScript");
+
+console.log("Updated to-do list:", toDoList);
---
+++ JsSubmissions/lakshmifg/Logic Building/FactorialCalculator.js
@@ -0,0 +1,21 @@
+function factorialLoop(num) {
+    if (num < 0) {
+      return "Factorial is not defined for negative numbers";
+    }
+  
+    if (num === 0 || num === 1) {
+      return 1;
+    }
+  
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  // Example usage:
+  console.log("factorial of 5",factorialLoop(5)); // Output: 120
+  console.log("factorial of 0",factorialLoop(0)); // Output: 1
+  console.log("factorial of 1",factorialLoop(1)); // Output: 1
+  console.log("factorial of -3",factorialLoop(-3)); // Output: Factorial is not defined for negative numbers
