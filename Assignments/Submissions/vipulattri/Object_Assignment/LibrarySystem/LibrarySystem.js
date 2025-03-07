d684c1f8376ef0d62103e4afb58d1c66117f2d1b
Vipul Attri <v-i-p-u-l@antix1>
2025-03-07 15:08:26 +0530

    Added JavaScript assignments by vipulattri

---
+++ Assignments/Submissions/vipulattri/Object_Assignment/LibrarySystem/LibrarySystem.js
@@ -0,0 +1,15 @@
+// now we are going to make an object named book
+
+let book = {
+    title:"Mahatma Gandhi Autobiography",
+    author:"Mahatma Gandhi",
+    isAvailable:true,
+}
+
+// now we are going to update the availability of the book
+console.log("Availability of the book is ");
+console.log(book.isAvailable)
+book.isAvailable = false;
+console.log("After the availability of the book is ");
+console.log(book.isAvailable)
+
