448d52b9c0e7b8fa0a07417b0b80d89b9bfec430
Mamatha <balagondamamatha23@gmail.com>
2025-03-10 11:51:40 +0530

    js

---
+++ js/variable/swap.js
@@ -0,0 +1,7 @@
+// initalize  a = 10 and  b = 40
+// Swap values using [a,b]=[b,a] here it is assigned a as b and b as a
+//print a,b values
+let a = 10, b = 40;
+[a, b] = [b, a];
+
+console.log("a:", a, "b:", b);
