a66fb99068a1855c55cb325d745a51a77210ca56
Mamatha <balagondamamatha23@gmail.com>
2025-03-10 11:45:07 +0530

    Removed js folder from main branch

--- js/variable/swap.js
+++
@@ -1,7 +0,0 @@
-// initalize  a = 10 and  b = 40
-// Swap values using [a,b]=[b,a] here it is assigned a as b and b as a
-//print a,b values
-let a = 10, b = 40;
-[a, b] = [b, a];
-
-console.log("a:", a, "b:", b);
