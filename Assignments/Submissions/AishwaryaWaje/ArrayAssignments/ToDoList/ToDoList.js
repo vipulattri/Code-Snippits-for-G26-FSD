dc8a9caa6d96bbd5a2b4d7975bffd2d639d5dd42
Aishwarya Waje <aishux2223@gmail.com>
2025-03-10 11:50:13 +0530

    Assignment Completed

---
+++ Assignments/Submissions/AishwaryaWaje/ArrayAssignments/ToDoList/ToDoList.js
@@ -0,0 +1,10 @@
+let ToDoList = [];
+ToDoList.push('Plant two trees');
+ToDoList.push('Donate blood');
+ToDoList.push('Serve two senior citizens');
+
+console.log(ToDoList);
+ToDoList.shift();
+console.log(ToDoList);
+ToDoList.push('Plant two trees');
+console.log(ToDoList);
