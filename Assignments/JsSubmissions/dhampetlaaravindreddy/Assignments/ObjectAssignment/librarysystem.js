2155467225f0aabad1c7e8b43c9b21629ed0b519
dhampetlaaravindreddy <aravindreddydhampetla@gmail.com>
2025-03-24 19:29:21 +0530

    assignments added

---
+++ Assignments/JsSubmissions/dhampetlaaravindreddy/Assignments/ObjectAssignment/librarysystem.js
@@ -0,0 +1,15 @@
+//Create an object book with properties: title, author, isAvailable. 
+// Write code to check if the book is available and update its isAvailable status. 
+// Print the updated object.
+
+let book = {
+    title: "The Ramayana",
+    author: "Valmiki",
+    isAvailable: true
+};
+
+if (book.isnotAvailable) {
+    book.isnotAvailable = false;
+}
+
+console.log(book);
