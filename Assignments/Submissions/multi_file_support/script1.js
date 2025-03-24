b3375d6dc720dc68b34d6785ae9760af04ce52ca
Akash <akashkr6060@gmail.com>
2025-03-24 20:34:35 +0530

    added multi file support

---
+++ Assignments/Submissions/multi_file_support/script1.js
@@ -0,0 +1,12 @@
+export function newFile(){
+    console.log("Inside newFile")
+}
+
+export function addition(a,b){
+    console.log("The result is:",20+60)
+}
+
+export default {
+    newFile,
+    addition
+}
