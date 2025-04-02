145a623b20234af9d2d868c2494be261af9408b7
ridhisingh06 <singhridhi60803@gmail.com>
2025-04-02 10:04:56 +0530

    Completed the password generator assignment

---
+++ Assignments/Submissions/Ridhi_Singh/ObjectAssignment/profile.js
@@ -0,0 +1,13 @@
+let user = {
+    name: "Ridhi singh",
+    age: 24,
+    city: "meerut"
+};
+
+user.age = 21;
+
+user.profession = "Full Stack Developer";
+
+delete user.city;
+
+console.log(user);
--- Assignments/Submissions/arrayassignment.js/removeduplicates.js
+++
@@ -1,6 +0,0 @@
-let arr = [3,5,3,5,3,5,3,5];
-
-function removeDuplicate(data) {
-    return [...new Set(data)];
-}
-console.log(removeDuplicate(arr));
