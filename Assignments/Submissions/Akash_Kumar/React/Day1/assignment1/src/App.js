8eeabc0a07004f7ee2ea9c9d97d08f13438a4342
Akash <akashkr6060@gmail.com>
2025-04-04 21:49:47 +0530

    day1 React Assignments

---
+++ Assignments/Submissions/Akash_Kumar/React/Day1/assignment1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="profile_pic"></div>
+      
+      <div className="important_details">
+        <div className="user_name">Akash Kumar</div>
+        <div className="contact_info">1234567890</div>
+      </div>
+      
+      <div className="about_me">I am pursuing Btech computer science and engineering at Shobhit institute of engineering and Technology</div>
+    </div>
+  );
+}
+
+export default App;
