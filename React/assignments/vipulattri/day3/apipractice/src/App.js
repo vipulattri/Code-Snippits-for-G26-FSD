6f5955753dd106276de5b5b20bdf50fbe051b510
vipulattri <attrivipul72@gmail.com>
2025-04-09 19:24:14 +0530

    day3 assignment

---
+++ React/assignments/vipulattri/day3/apipractice/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Api from './components/Api'
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 style={{textAlign:'center'}}>API Practice</h1> 
+      </div>
+      <Api/>
+
+    </>
+  )
+}
+
+export default App
