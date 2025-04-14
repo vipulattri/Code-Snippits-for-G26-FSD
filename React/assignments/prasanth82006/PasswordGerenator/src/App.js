be47f86354f90c3e932504c2ca53c55a0b9a0f61
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-04-14 11:41:20 +0530

    Create App.js

---
+++ React/assignments/prasanth82006/PasswordGerenator/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Password from "./component/Password";
+
+function App() {
+  return (
+    <div className="app">
+      <Password />
+    </div>
+  );
+}
+
+export default App;
