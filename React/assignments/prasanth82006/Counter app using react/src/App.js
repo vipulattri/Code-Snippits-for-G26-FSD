82e73396b2bc3f45434f02c978a0766f97e2e6e1
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-04-14 11:37:34 +0530

    Create App.js

---
+++ React/assignments/prasanth82006/Counter app using react/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counterapp from "./component/Counterapp";
+
+function App() {
+  return (
+    <div className="app">
+      <Counterapp/>
+    </div>
+  );
+}
+
+export default App;
