8e95286187d3fdcbe1809334a2d9576853de0a51
Akash <akashkr6060@gmail.com>
2025-04-15 18:40:34 +0530

    Finished the Counter App

---
+++ Assignments/Submissions/Akash_Kumar/React/Day2/counter_app/src/counter.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function counter(){
+    <div id="main_container">
+
+        <div id="counterstatus"> 0 </div>
+
+        <div id="buttons">
+            <button id="increment">+</button>
+            <button id="decrement">-</button>
+        </div>
+
+    </div>
+    
+}
+
+export default counter;
