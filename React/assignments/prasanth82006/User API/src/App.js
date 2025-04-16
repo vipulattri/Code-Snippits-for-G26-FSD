41fe38209fa4d841deeb7c94f412e9ff06d697a2
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2025-04-16 11:44:15 +0530

    Create App.js

---
+++ React/assignments/prasanth82006/User API/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Userapi from "./component/Userapi";
+
+function App() {
+  return (
+    <div className="app">
+      <Userapi/>
+    </div>
+  );
+}
+
+export default App;
