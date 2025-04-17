d27d6ae10e1024dd1863091d16a0cef55a7873cb
vipulattri <attrivipul72@gmail.com>
2025-04-17 07:50:49 +0530

    added counter assignment

---
+++ React/assignments/vipulattri/day8/counter/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './components/Counter'
+import './App.css'
+const App = () => {
+  return (
+    <>
+    <h1 className='h1' style={{textAlign:'center' ,color:'blue', fontSize:'45px'}}>Counter App Using React Js</h1>
+    <Counter/>
+    
+    </>
+  )
+}
+
+export default App
