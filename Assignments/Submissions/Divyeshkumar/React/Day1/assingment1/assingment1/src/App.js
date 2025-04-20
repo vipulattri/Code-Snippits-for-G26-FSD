2f855e8f2dcbe5d428190b0b1a661ad7ef973a2d
divesh <divyeshkumar6658@gmail.com>
2025-04-21 00:23:32 +0530

    Assingment1 profile card

---
+++ Assignments/Submissions/Divyeshkumar/React/Day1/assingment1/assingment1/src/App.js
@@ -0,0 +1,20 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="profile_pic"></div>
+      
+      <div className="important_details">
+        <div className="user_name">Divyesh Kumar</div>
+        <div className="contact_info">0000000000</div>
+      </div>
+      
+      <div className="about_me"> Pursuing Btech Computer Science and Engineering at Shobhit institute of engineering and Technology</div>
+    </div>
+  );
+}
+
+export default App;
