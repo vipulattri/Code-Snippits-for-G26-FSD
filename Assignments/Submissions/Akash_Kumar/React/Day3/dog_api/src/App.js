5498ddbef18b7e4e4ca63ad2d5159ac228c2a7c2
Akash <akashkr6060@gmail.com>
2025-05-02 21:24:02 +0530

    Completed the dog api call

---
+++ Assignments/Submissions/Akash_Kumar/React/Day3/dog_api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RandomDog from './api';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomDog/>
+    </div>
+  );
+}
+
+export default App;
