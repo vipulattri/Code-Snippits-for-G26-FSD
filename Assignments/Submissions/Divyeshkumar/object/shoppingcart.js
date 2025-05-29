b63a55cf77bb1e883f22640ffd9e99609675473e
Divyesh kumar <divyeshkumar6658@gmail.com>
2025-05-29 12:08:02 +0530

    Assingments react Day1 to Day 5 (#119)
    * Dogesh bro
    
    * bewakoof-by-Divyesh
    
    * update
    
    * update
    
    * Finished assignments

---
+++ Assignments/Submissions/Divyeshkumar/object/shoppingcart.js
@@ -0,0 +1,17 @@
+//Create an object cart with itemName, quantity, and price. Update the quantity and calculate the total cost (quantity x price). Add a discount property and print the final object.
+
+let cart = {
+    itemName: "litchi",
+    quantity: 4,
+    price: 19
+};
+
+cart.quantity = 35;
+let totalCost = cart.quantity * cart.price;
+cart.discount = 5;
+console.log(cart);
+console.log("Total Cost after discount: ", totalCost-cart.discount);
+
+// Output:
+// { itemName: 'litchi', quantity: 30, price: 20, discount: 5 }
+// Total Cost after discount:  595
---
+++ React/assignments/DivyeshKumar/Day4/dog-api/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Dog from "./Dog";
+
+function App() {
+  return (
+    <div className="app">
+      <Dog/> // renders the dog component 
+    </div>
+  );
+}
+
+export default App;
